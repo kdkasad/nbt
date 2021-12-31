3092b138967b3ed69bcf7571c14446a9d12b7fef
Kian Kasad <kian@kasad.com>
2021-12-31 15:49:12 -0800

    Implement structured command-line option parsing
    Uses getopt(3) to parse command-line options. This allows configurable
    settings to be added later on.
    
    Also adds printing of usage information.

---
+++ src/cfg.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2021 Kian Kasad
+ *
+ * This file is distributed under a modified BSD license. See the provided
+ * LICENSE file for details.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause-Patent
+ */
+
+#ifndef _CFG_H_
+#define _CFG_H_
+
+#include <stdbool.h>
+#include <stdio.h>
+
+struct nbt_config {
+	FILE *in;
+};
+
+#endif /* _CFG_H_ */
--- src/main.c
+++ src/main.c
@@ -9,28 +9,76 @@
 
 #include <error.h>
 #include <errno.h>
+#include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "cfg.h"
 #include "err.h"
 #include "print.h"
 #include "tag.h"
 
+static void print_usage(const char *argv0);
+
+static struct nbt_config cfg = {
+	.in = NULL,
+};
+
 int main(int argc, char *argv[])
 {
-	FILE *f = stdin;
-	if (argc >= 2) {
-		f = fopen(argv[1], "rb");
-		if (!f)
-			error_at_line(RET_USER_ERROR, errno, __FILE__, __LINE__ - 2, "error opening file");
+	int c;
+	int err = 0; /* if nonzero, error ocurred parsing options */
+	static const char *optstring = ":h";
+	while ((c = getopt(argc, argv, optstring)) != -1) {
+		switch (c) {
+		case 'h':
+			print_usage(argv[0]);
+			exit(RET_NONE);
+			break;
+		case ':':
+			error_at_line(RET_NONE, 0, __FILE__, __LINE__ - 1, "option '-%c' requires an argument", optopt);
+			err++;
+			break;
+		case '?':
+			error_at_line(RET_NONE, 0, __FILE__, __LINE__ - 1, "unrecognized option '-%c'", optopt);
+			err++;
+			break;
+		}
 	}
 
-	struct tag *tag = read_tag(f);
+	/* exit after parsing all options so all errors are printed to user */
+	if (err)
+		exit(RET_USER_ERROR);
+
+	/* input file given */
+	if (optind < argc) {
+		if (!(cfg.in = fopen(argv[optind], "rb")))
+			error_at_line(RET_RUNTIME_ERROR, errno, __FILE__, __LINE__ - 1, "%s", argv[optind]);
+		optind++;
+	} else {
+		cfg.in = stdin;
+	}
+
+	/* attempt to read one tag */
+	struct tag *tag = read_tag(cfg.in);
 	if (!tag)
 		error_at_line(RET_RUNTIME_ERROR, 0, __FILE__, __LINE__ - 2, "no tag was read");
-	print_tag(tag);
-	free(tag);
 
+	print_tag(tag);
+
+	free(tag);
 	return 0;
+}
+
+void print_usage(const char *argv0)
+{
+	printf(
+"Usage:\n"
+"\t%s [options] [infile [outfile]]\n"
+"\n"
+"Options:\n"
+"	-h	Print this usage information\n"
+	, argv0);
 }
